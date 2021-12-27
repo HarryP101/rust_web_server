f01b9f746ab2d800ed5eb192dde14c116bf84171
Harry Prudden <htprudden@gmail.com>
2021-12-27 11:36:35 +0000

    Refactor code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rust_web_server
+Developing a multi threaded web server to learn the rust programming language
--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,21 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-    
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
-    else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
 
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
